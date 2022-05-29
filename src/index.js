965bcb5f2e481ba86cf362f0292b641096254931
Lauri Kääriäinen <4522622+lauri-kaariainen@users.noreply.github.com>
2022-05-29 15:06:10 +0300

    add city selection at the start

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import "./style";
-import { render } from "preact";
+import { Fragment, render } from "preact";
 import { useState, useEffect } from "preact/hooks";
 import { Restaurant } from "./restaurant";
 import { ConfirmDialog } from "./confirmdialog";
+import Autocomplete from "@mui/material/Autocomplete";
 import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import citiesObj from "./cities.json";
 
-const city = "tampere";
-const SEARCH =
-  "//lauri.space/solidabiskoodihaaste22/api/v1/restaurants/" + city;
+// const city = "tampere";
+const searchUrl = "//lauri.space/solidabiskoodihaaste22/api/v1/restaurants/";
 
 const App = () => {
   const [restaurants, setRestaurants] = useState([]);
   const [activeVote, setActiveVote] = useState("");
   const [proposedVote, setProposedVote] = useState("");
   const [openDialog, setOpenDialog] = useState(false);
+  const [selectedCity, setSelectedCity] = useState("");
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
-    fetch(`${SEARCH}`)
-      .then((r) => r.json())
-      .then((json) => setRestaurants((json && json.restaurants) || []));
-  }, [city]);
+    if (selectedCity)
+      fetch(`${searchUrl + selectedCity}`)
+        .then((r) => r.json())
+        .then((json) => setRestaurants((json && json.restaurants) || []));
+  }, [selectedCity]);
 
   const voteRestaurant = (name) => {
-    // console.log(name);
     setProposedVote(name);
     setOpenDialog(true);
   };
@@ -35,16 +38,32 @@ const App = () => {
 
   return (
     <Container maxWidth="sm">
-      <h1>{city.toUpperCase()}</h1>
-      <div class="list">
-        {restaurants.map((restaurant) => (
-          <Restaurant
-            restaurant={restaurant}
-            selected={restaurant.name === activeVote}
-            clickVote={voteRestaurant.bind(null, restaurant.name)}
-          />
-        ))}
-      </div>
+      <h1>Lounaspaikkaäänestys</h1>
+      <Autocomplete
+        disablePortal
+        blurOnSelect
+        id="combo-box-demo"
+        options={citiesObj.cities}
+        sx={{ width: 300 }}
+        onChange={(_, newVal) => setSelectedCity(newVal)}
+        renderInput={(params) => <TextField {...params} label="Kaupunki" />}
+      />
+      {selectedCity ? (
+        <Fragment>
+          <h1>{selectedCity.toUpperCase()}</h1>
+          <div class="list">
+            {restaurants.map((restaurant) => (
+              <Restaurant
+                restaurant={restaurant}
+                selected={restaurant.name === activeVote}
+                clickVote={voteRestaurant.bind(null, restaurant.name)}
+              />
+            ))}
+          </div>
+        </Fragment>
+      ) : (
+        ""
+      )}
       <ConfirmDialog
         handleClose={handleDialogClose}
         openDialog={openDialog}
